0e4a9bea9fabd6b5ad5a337123883cef13fb102d
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2025-03-11 19:26:35 -0500

    feature: Database connection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const express = import ('express');
+const express = require("express");
 require("dotenv").config();
+const connectionDB = require("./config/database");
 
-const app = process.env.PORT || 3005;
-console.log(app);
+const app = express();
+const port = process.env.PORT || 3005;
+
+// listen: 2 param puerto y función flecha
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+connectionDB();
