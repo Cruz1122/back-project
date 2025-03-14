15fa8d3d99d9f20403c91fbca90f00f6de2ffb97
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2025-03-14 12:17:27 -0500

    fix: AuthController now exports sign modules

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,29 +1,14 @@
-const {PrismaClient} = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const signUp = async (req, res) => {
-    const {fullName, email, password} = req.body;
-    const user = await prisma.user.create({
-        data: {
-            fullName,
-            email,
-            password
-        }
-    });
-    
-    res.status(201).json(user);
+  const { fullname, email, current_password } = req.body;
+  console.log(req.body);
 };
-const signIn = async (req, res) => {
-    const {email, password} = req.body;
-    const user = await prisma.user.findFirst({
-        where: {
-            email,
-            password
-        }
-    });
-    if (user) {
-        res.status(200).json({message: 'User found'});
-    } else {
-        res.status(404).json({message: 'User not found'});
-    }
+
+const signIn = async (req, res) => {};
+
+module.exports = {
+  signUp,
+  signIn,
 };
