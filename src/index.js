0b2fd646a1faca340b091b31f999142a9dc34fa5
Camilo Cruz <juan.cruz37552@ucaldas.edu.co>
2025-04-01 20:43:55 -0500

    feat. Login component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,20 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const connectionDB = require("./config/database");
 const routes = require("./routes/routes");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3005;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, // Permitir todas las solicitudes de origen
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"], // Métodos permitidos
+    allowedHeaders: ["Content-Type", "Authorization"], // Encabezados permitidos
+  })
+); // Habilitar CORS para todas las rutas
+
+app.options("*", cors()); // Permitir preflight requests para todas las rutas
 
 // listen: 2 param puerto y función flecha
 app.listen(port, () => {
